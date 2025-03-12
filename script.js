9a276049e595e1f5cc13ce039ddbb04e4edfdfe8
Bartosz <117040452+brukselek@users.noreply.github.com>
2025-03-12 21:38:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById('display');
+function appendToDisplay(value){
+   display.value += value;
+}
+function clearDisplay(){
+   display.value = '';
+}
+function deleteFromDisplay(){
+   display.value = display.value.slice(0, -1);
+}
+function calculate(){
+   try{
+      display.value = eval(display.value);
+   }
+   catch(e){
+      display.value = 'Error';
+   }
+}
