8cb3fe2061109c75863e08b2925863d16d05bbb2
Bartosz <117040452+brukselek@users.noreply.github.com>
2025-03-12 22:01:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ function calculate(){
       display.value = 'Error';
    }
 }
+function handleSqrt(){
+   let num = eval(display.value);
+
+   if(num < 0){
+      display.value = "Error"
+   }
+   else{
+      display.value = Math.sqrt(num);
+   }
+}
+function handlePercentages(){
+   if(display.value != ''){
+      display.value = eval(display.value) / 100;
+   }
+}
